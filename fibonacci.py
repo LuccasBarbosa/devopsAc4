092549329091f0cb4457cd8d6ab77c70556b23c3
Luccas Barbosa <luccas@mrdavinci.com.br>
2020-10-19 22:21:19 -0300

    Docker e fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def nao_entre_em_panico():
+    anterior = 0
+    proximo = 1
+    i = 0
+    resposta = '1, '
+
+    while(i < 50):
+        proximo = proximo + anterior
+        anterior = proximo - anterior
+        i += 1
+
+        if(proximo == 0):
+            proximo = proximo + 1    
+
+        resposta+= str(proximo) + ", "
+
+
+    return resposta
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
